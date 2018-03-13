769fd09592d6693fe820ede5191e1a2c870a4218
Huxx-j(장성우) <perfekta213@gmail.com>
2018-03-13 21:36:16 +0900

    리턴 수정

--- src/com/javaex/problem05/StringUtil.java
+++ src/com/javaex/problem05/StringUtil.java
@@ -3,8 +3,8 @@ package com.javaex.problem05;
 public class StringUtil {
 
     public static String concatString(String[] strArray){
-       return strArray[0].concat(strArray[1]).concat(strArray[2]);
-
+        String result = strArray[0].concat(strArray[1]).concat(strArray[2]);
+        return result;
 
     }
 
